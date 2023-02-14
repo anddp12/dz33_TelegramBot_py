20019914a415d4f0ee357684a076ed103b6310b6
anddp12 <anddp12@gmail.com>
2023-02-14 23:17:15 +0200

    selection words depending on the end of the word

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}\nИграем в игру города Украины.\nДля старта введите название города.".format(message.from_user))
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}\nИграем в слова - города Украины.\nДля старта введите название города.".format(message.from_user))
 
 
 with open("city_Ukr.json", 'r', encoding='utf-8') as f:
@@ -21,12 +21,28 @@ def echo_message(message):
         data.remove(message.text)
 
         for i in data:
-            if str(message.text[-1]).upper() == str(i[0]).upper():
-                bot.send_message(message.chat.id,i)
-                data.remove(i)
-                break
+            if message.text[-1] == "ь":
+                if str(message.text[-2]).upper() == str(i[0]).upper():
+                    bot.send_message(message.chat.id,i)
+                    data.remove(i)
+                    break
+            elif message.text[-1] == "ы":
+                if str(message.text[-2]).upper() == str(i[0]).upper():
+                    bot.send_message(message.chat.id,i)
+                    data.remove(i)
+                    break
+            elif message.text[-1] == "й":
+                if str(message.text[-2]).upper() == str(i[0]).upper():
+                    bot.send_message(message.chat.id,i)
+                    data.remove(i)
+                    break
+            else:
+                if str(message.text[-1]).upper() == str(i[0]).upper():
+                    bot.send_message(message.chat.id,i)
+                    data.remove(i)
+                    break
     else:
-        bot.send_message(message.chat.id, "Слово задублировано")
+        bot.send_message(message.chat.id, "Слово задублировано или не верно введен город")
 
 
 bot.infinity_polling()
